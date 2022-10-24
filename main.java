d48467b40afccc646217436e0277d9949e4b8b03
f-erm <62855603+f-erm@users.noreply.github.com>
2022-10-24 13:48:09 +0200

    start

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+	
+	int value;
+	List<Node> neighbors;
+
+	public Node(int value) {
+		this.value = value;
+		neighbors = new ArrayList<>();
+	}
+	
+	public void addEdge(Node to) {
+		neighbors.add(to);
+	}
+
+
+
+}class Main {
+    public static void main(String[] args) {
+    }
